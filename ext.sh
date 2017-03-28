58d0967fdeb374c85d92914077da2149b4c87753
Michael Stapelberg <michael@stapelberg.de>
2017-03-28 19:08:14 +0200

    folds

--- ext.sh
+++ ext.sh
@@ -3,7 +3,14 @@
 # complete the previous command
 travis_time_finish
 
+travis_fold start nesting
 travis_cmd 'echo here i am, nesting' --echo --timing
 travis_result $?
+travis_fold end nesting
+
+travis_fold start second
+travis_cmd 'echo second command' --echo --timing
+travis_result $?
+travis_fold end second
 
 travis_time_start
